bd6a736cf6c9fc5b8f542951f1668a7bca679f5a
Samuel Souza <101839733+devsamuel23@users.noreply.github.com>
2023-05-23 20:29:30 -0300

    index.js

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,89 +1,83 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable no-unused-vars */
-/* eslint-disable react-hooks/rules-of-hooks */
-// eslint-disable-next-line no-unused-vars
-import { useEffect, useState } from "react";
-import { useParams, useNavigate, json } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import api from "../../services/api";
 import "./filme-info.css";
 import { toast } from "react-toastify";
 
-function filme() {
+function Filme() {
   const { id } = useParams();
   const navigation = useNavigate();
-  const [filme, setFilmes] = useState({});
+  const [filme, setFilme] = useState({});
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilme() {
-      await api
-        .get(`/movie/${id}`, {
+      try {
+        const response = await api.get(`/movie/${id}`, {
           params: {
             api_key: "7add4681ffb4849375f1ea15840f0d3a",
             language: "pt-BR",
           },
-        })
-        .then((response) => {
-          setFilmes(response.data);
-        })
-        .catch(() => {
-          setLoading(false);
-          navigation("/", { replace: true });
-          return;
         });
+        setFilme(response.data);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        navigation("/", { replace: true });
+      }
     }
+
     loadFilme();
 
     return () => {
       console.log("componente desmontado");
     };
-
-    // eslint-disable-next-line no-unreachable
-    if (loading) {
-      return (
-        <div className="filme-info">
-          <h1>Carregando detalhes...</h1>
-        </div>
-      );
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [navigation, id]);
 
-  function salverFilme(){
+  function salvarFilme() {
     const minhaLista = localStorage.getItem("@primeflix");
-
     let filmesSalvos = JSON.parse(minhaLista) || [];
 
-    const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
+    const hasFilme = filmesSalvos.some(
+      (filmeSalvo) => filmeSalvo.id === filme.id
+    );
 
-    if(hasFilme){
-        toast.warn(`Filme já foi salvo na sua lista!`);
-        return;
+    if (hasFilme) {
+      toast.warn(`Filme já foi salvo na sua lista!`);
+      return;
     }
 
     filmesSalvos.push(filme);
-    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos))
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
     toast.success(`Filme salvo com sucesso!`);
+  }
 
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
   }
 
   return (
     <div className="filme-info">
       <h1>{filme.title}</h1>
-      <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} />
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
 
-      <h3>sinopse</h3>
+      <h3>Sinopse</h3>
       <span>{filme.overview}</span>
       <strong>Avaliação: {filme.vote_average} /10</strong>
 
       <div className="area-buttons">
-        <button onClick={salverFilme}>Salvar</button>
+        <button onClick={salvarFilme}>Salvar</button>
         <button>
           <a
-            target="blanck"
-            rel="external"
+            target="_blank"
+            rel="noopener noreferrer"
             href={`https://youtube.com/results?search_query=${filme.title} Trailer`}
           >
             Trailer
@@ -94,4 +88,4 @@ function filme() {
   );
 }
 
-export default filme;
+export default Filme;
